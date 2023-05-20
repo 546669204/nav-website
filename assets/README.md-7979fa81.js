66aa77c8eed8a0eda0fe84a9d3d343e51185a148
546669204 <546669204@users.noreply.github.com>
2023-05-20 11:52:31 +0000

    deploy: 8728c75c8de1cccb61d970cca8a236ccbef81469

--- assets/README.md-7979fa81.js
+++
@@ -1,7 +0,0 @@
-import"./index-fa1c11f5.js";import{c as t}from"./esm-8c7c145d.js";const o=[],a={toc:o},r="wrapper";function n({components:e,...s}){return t(r,{...a,...s,components:e,mdxType:"MDXLayout"},t("p",null,"dasds123213qwewq"),t("p",null,`sadas
-das
-d
-as
-d
-s
-111  `),t("p",null,"3"))}n.isMDXComponent=!0;const i={},u=void 0,l={unversionedId:"README",id:"README",title:"README",description:"dasds123213qwewq",source:"@site/docs/README.md",sourceDirName:".",slug:"/",permalink:"/nav-website/docs/",draft:!1,editUrl:"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/docs/README.md",tags:[],version:"current",frontMatter:{},sidebar:"tutorialSidebar",previous:{title:"README",permalink:"/nav-website/docs/CSS/"},next:{title:"cron GUI",permalink:"/nav-website/docs/cron/gui"}},p={},m=[];export{p as assets,u as contentTitle,n as default,i as frontMatter,l as metadata,m as toc};
